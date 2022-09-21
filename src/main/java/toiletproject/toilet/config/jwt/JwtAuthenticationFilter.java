50665dce75cacc8b5e3184aae482b453a3c8e973
Kwon <anstjdrnjs3@gmail.com>
2022-09-21 14:08:50 +0900

    Add: JWT 토큰 인증 필터 등록

---
+++ src/main/java/toiletproject/toilet/config/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,36 @@
+package toiletproject.toilet.config.jwt;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class JwtAuthenticationFilter extends GenericFilterBean {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        if (token == null) request.setAttribute("exception", "인증되지 않은 계정입니다.");
+        else if(jwtTokenProvider.validateToken(token, (HttpServletRequest) request)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        chain.doFilter(request, response);
+    }
+}
