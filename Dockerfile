0b3e1a7bf12646f48fd9f219cb580f13e78d110a
Kwon <anstjdrnjs3@gmail.com>
2022-10-07 11:51:44 +0900

    Update: CICD 설정

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM openjdk:11-jdk
 ARG JAR_FILE=./build/libs/toilet-0.0.1-SNAPSHOT.jar
+ENV ENV_IDLE_PROFILE=$IDLE_PROFILE
 COPY ${JAR_FILE} app.jar
-EXPOSE 5000
-ENTRYPOINT ["java","-jar","/app.jar"]
+RUN echo $ENV_IDLE_PROFILE
+ENTRYPOINT ["java","-jar","/app.jar", "--spring.profiles.active=${ENV_IDLE_PROFILE}"]
--- src/main/java/toiletproject/toilet/config/SecurityConfig.java
+++ src/main/java/toiletproject/toilet/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 .authorizeRequests()
                 .antMatchers("/api/users/register", "/api/users/login",
                         "/api/users/check_email", "/api/users/upload", "/api/users/redirect",
-                "/api/users/reset_password").permitAll()
+                "/api/users/reset_password", "/actuator/health").permitAll()
                 .anyRequest().authenticated()
 
                 .and()
