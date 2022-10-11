9f47e93e9001554fc6ae9d0ec4e49357b3f868d6
Kwon <anstjdrnjs3@gmail.com>
2022-10-11 18:07:40 +0900

    Update: 주변 화장실 정보 native 쿼리 사용

--- src/main/java/toiletproject/toilet/toilets/ToiletRepository.java
+++ src/main/java/toiletproject/toilet/toilets/ToiletRepository.java
@@ -1,10 +1,37 @@
 package toiletproject.toilet.toilets;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import toiletproject.toilet.toilets.entity.ToiletEntity;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ToiletRepository extends JpaRepository<ToiletEntity, Long> {
     Optional<ToiletEntity> findByAddress(String address);
+
+    @Query(nativeQuery = true,
+            value = "SELECT\n" +
+        "          t.*, (\n" +
+        "          6371 * acos (\n" +
+        "          cos ( radians(:lat) )\n" +
+        "          * cos( radians( t.lat ) )\n" +
+        "          * cos( radians( t.lng ) - radians(:lng) )\n" +
+        "          + sin ( radians(:lat) )\n" +
+        "          * sin( radians( t.lat ) )\n" +
+        "          )\n" +
+        "          ) AS distance,\n" +
+        "          o.*\n" +
+        "          FROM toilet_db.toilets as t\n" +
+        "          LEFT OUTER JOIN toilet_db.options as o\n" +
+        "          ON t.option_id = o.id\n" +
+        "          HAVING distance < :dist\n" +
+        "          ORDER BY distance\n" +
+        "          LIMIT 0, 20;"
+    )
+    List<ToiletEntity> aroundToilet(
+            @Param("lat") double lat,
+            @Param("lng") double lng,
+            @Param("dist") double dist);
 }
